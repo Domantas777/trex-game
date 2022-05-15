56d1fdfc33e1cd36ba468f33ba45af27353ad238
markizas <domantas.markevicius@gmail.com>
2022-05-15 23:20:32 +0300

    Created homepage with css and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from './pages/Homepage';
+import HomePage from "./pages/Homepage/Homepage";
+import Store from "./pages/Store/Store";
+import Achievements from "./pages/Achievements/Achievements";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="store" element={<Store />} />
+        <Route path="achievements" element={<Achievements />} />
       </Routes>
     </Router>
   );
