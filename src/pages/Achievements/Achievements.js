bd7dfbaaeb223adc1a966349c7243f63727bff78
markizas <domantas.markevicius@gmail.com>
2022-05-16 23:50:44 +0300

    Added achievements page

--- src/pages/Achievements/Achievements.js
+++ src/pages/Achievements/Achievements.js
@@ -1,5 +1,21 @@
+import styles from "./Achievements.module.css";
+import AchievementsBox from "../../components/AchievementBox";
+
 function Achievements() {
-  return <div>Achievements</div>;
+  return (
+    <div className={styles.container}>
+      <span className={styles.title}>Achievements</span>
+      <div className={styles.box}>
+        <AchievementsBox title="Newbie" text="Play the game 10 times" />
+        <AchievementsBox title="Advanced" text="Play the game 20 times" />
+        <AchievementsBox title="Pro" text="Play the game 30 times" />
+        <AchievementsBox title="True gamer" text="Get a highscore of 5000" />
+        <AchievementsBox title="bruh" text="eat my ass" />
+        <AchievementsBox title="bruh" text="eat my ass" />
+        <AchievementsBox title="bruh" text="eat my ass" />
+      </div>
+    </div>
+  );
 }
 
 export default Achievements;
